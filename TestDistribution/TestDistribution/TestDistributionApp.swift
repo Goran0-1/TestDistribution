09118df5ce45be90aface9fea109dcf8cc49ed50
Goran Tatomirov <goran.tatomirov@vegait.rs>
2024-05-13 14:58:34 +0200

    Real project

---
+++ TestDistribution/TestDistribution/TestDistributionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDistributionApp.swift
+//  TestDistribution
+//
+//  Created by Goran Tatomirov on 13.5.24..
+//
+
+import SwiftUI
+
+@main
+struct TestDistributionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
